d4f681ab83b8edba168bb29b3e80aa370c1a88a5
KaiqueVA <kaique_x_@hotmail.com>
2025-05-05 18:28:07 -0300

    feat: Adiciona página de registro e lógica de cadastro de usuário

---
+++ Frontend/js/register.js
@@ -0,0 +1,37 @@
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+  
+    const data = {
+      email: document.getElementById("email").value,
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+      first_name: document.getElementById("firstName").value,
+      last_name: document.getElementById("lastName").value,
+    };
+  
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/register/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+      });
+  
+      const result = await response.json();
+  
+      if (response.ok) {
+        document.getElementById("registerMessage").textContent = "Cadastro realizado com sucesso! Redirecionando...";
+        
+        // Aguarda 1.5 segundos e redireciona para login
+        setTimeout(() => {
+          window.location.href = "../Pages/login.html"; // ou "../Pages/login.html" se necessário
+        }, 500);
+      } else {
+        document.getElementById("registerMessage").textContent =
+          `Erro: ${result.detail || "verifique os dados preenchidos"}`;
+      }
+    } catch (err) {
+      document.getElementById("registerMessage").textContent = "Erro de conexão.";
+      console.error(err);
+    }
+  });
+  
