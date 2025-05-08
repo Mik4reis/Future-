8208f1ad6fa7b4286ab214880960c3bb9afc4720
KaiqueVA <kaique_x_@hotmail.com>
2025-05-08 16:55:05 -0300

    feat: Atualiza a documentação da API de login para incluir informações sobre doações

--- Backend/login_api/login_api/urls.py
+++ Backend/login_api/login_api/urls.py
@@ -5,17 +5,20 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Login API",
-      default_version='v1',
-      description="API de login segura com Django REST",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Future- Backend API",
+        default_version='v1',
+        description="Documentação das APIs de login e doações",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    # NÃO passe `security` aqui!
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('admin/', admin.site.urls),
+   path('api/', include('users.urls')),
+   path('api/', include('donations.urls')),
+   path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
