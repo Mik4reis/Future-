8613b61058dd91117fbc494cb3447d55c035f3b8
KaiqueVA <kaique_x_@hotmail.com>
2025-05-10 16:31:25 -0300

    feat: Adiciona serializers para gerenciamento de usuários e alteração de senha

--- Backend/login_api/users/serializers.py
+++ Backend/login_api/users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth import authenticate
+from .models import CustomUser as User
+from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -25,3 +27,21 @@ class LoginSerializer(serializers.Serializer):
         if user is None:
             raise serializers.ValidationError("Credenciais inválidas")
         return {'user': user}
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Para GET/PUT/PATCH/DELETE em /api/users/me/
+    """
+    class Meta:
+        model  = User
+        fields = ('id', 'email', 'first_name', 'last_name')
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(
+        write_only=True,
+        validators=[validate_password]
+    )
+    
